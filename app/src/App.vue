6a7ce48cd7eb1caf431d163fc3c872606ac6c9f3
张坤 <dreamboys900828@gmail.com>
2019-08-01 16:32:06 +0800

    重置完毕

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <elm-header></elm-header>
     <router-view/>
+    <elm-footer></elm-footer>
   </div>
 </template>
 
 <script>
+import ElmHeader from "./components/elmHeader/elmHeader";
+import ElmFooter from "./components/elmFooter/elmFooter";
 export default {
-  name: 'App'
+  name: 'App',
+  components: {ElmFooter, ElmHeader}
 }
 </script>
 
---
+++ app/src/components/elmHeader/elmHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="elm-header">
+      <h1>这是头部</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "elmHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
