5f2b13db29503b750b870305fa003875731a0e85
张坤 <dreamboys900828@gmail.com>
2019-08-02 16:24:10 +0800

    查询商店

---
+++ app/src/components/searchGood/searchGood.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="searchGood">
+        <h1>这里是搜索商店页面</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'searchGood'
+    };
+</script>
+<style scoped lang="less">
+  @import "_searchGood";
+</style>
