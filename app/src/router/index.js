419c9080c9034f0a311fcc41092ae2ce513500f7
张坤 <dreamboys900828@gmail.com>
2019-08-02 16:26:32 +0800

    更改路由

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,10 +2,37 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 Vue.use(Router);
+// 选择城市
 import elmNavigation from '../components/elmNavigation/elmNavigation';
+// 查询城市
+import searchCity from '../components/searchCity/searchCity';
+
+
+// 外卖容器主页面
+import MainP from '../components/mainP/mainP';
+// 外卖主页附近商家
+import mSite from '../components/msite/msite';
+// 搜索商店
+import searchGood from '../components/searchGood/searchGood';
+// 我的订单
+import myOrder from '../components/order/order';
+// 个人中心
+import Mine from '../components/mine/mine';
+
 export default new Router({
   routes: [
     {path: '/', redirect: '/home'},
-    {path: '/home', component: elmNavigation}
+    {path: '/home', component: elmNavigation},
+    {path: '/searchCity', name: 'searchCity', component: searchCity},
+    {
+      path: '/mainP',
+      component: MainP,
+      children: [
+        {path: 'msite', component: mSite},
+        {path: 'searchgood', component: searchGood},
+        {path: 'order', component: myOrder},
+        {path: 'mine', component: Mine},
+      ]
+    }
   ]
 })
