1430a1ca3194d79f01f9edcdb801e56ecba1c1bb
张坤 <dreamboys900828@gmail.com>
2019-08-02 16:23:57 +0800

    订单首页

---
+++ app/src/components/order/order.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="order">
+        <h1>这里是订单页面</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'order'
+    };
+</script>
+<style scoped lang="less">
+  @import "_order";
+</style>
