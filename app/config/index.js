62cc4d439029e0d049733a256e0b181c7fc9443e
张坤 <dreamboys900828@gmail.com>
2019-08-02 16:26:48 +0800

    配置文件

--- app/config/index.js
+++ app/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/v1': {
+      '/': {
         target: 'https://elm.cangdu.org/',
         changeOrigin: true
       }
