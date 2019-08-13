9c7fd412bfee6677a3b367378651911d6b19a198
张坤 <dreamboys900828@gmail.com>
2019-08-13 16:00:15 +0800

    底部组件

---
+++ app/src/components/bar/bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bar">
+    <i class="iconfont arrow-left" @click="goBack">&#xe643;</i>
+    <div class="title">{{title}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String
+      }
+    },
+    name: 'bar',
+    methods: {
+      goBack() {
+        this.$router.go(-1);
+      }
+    },
+  };
+</script>
+<style scoped lang="less">
+  @import "_bar";
+</style>
