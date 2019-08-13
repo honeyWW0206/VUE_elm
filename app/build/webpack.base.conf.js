26d0469b0389cef14bba8c5e584f570b4000a830
张坤 <dreamboys900828@gmail.com>
2019-08-13 15:58:57 +0800

    配置文件

--- app/build/webpack.base.conf.js
+++ app/build/webpack.base.conf.js
@@ -4,16 +4,15 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
-
 
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/mainP.js'
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -68,6 +67,10 @@ module.exports = {
       {
         test: /\.less$/,
         loader: "style-loader!css-loader!less-loader"
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: 'style!css!sass'
       }
     ]
   },
